ed9986d077c5c7c3d0acc372432cbc9f3482cfdf
OSA24021987 <spallinum@gmail.com>
2025-01-23 22:38:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+// Задача №1.
+/*
+let first_Number = prompt('Введите первое число!');
+let second_Number = prompt('Введите второе число!');
+first_Number = +first_Number;
+second_Number = +second_Number;
+function comparing_Numbers(first_Number, second_Number) {
+    if (first_Number > second_Number) {
+        return 1;
+    }
+    else if (first_Number < second_Number) {
+        return -1;
+    }
+    else { return 0; }
+}
+
+alert(comparing_Numbers(first_Number, second_Number));
+*/
+//--------------------------------------------------------------------------
+
+// Задача №2.
+/*
+let user_Number = prompt('Введите число!');
+let rezult = 1;
+function calculating_The_Factorial(user_Number) {
+    for (let i = 1; i <= user_Number; i++) {
+        rezult = i * rezult;
+    }
+    return rezult;
+}
+
+alert(calculating_The_Factorial(user_Number));
+*/
+//---------------------------------------------------------------------
+
+// Задача №3.
+/*
+let first_Number = prompt('Введите первое число!');
+let second_Number = prompt('Введите второе число!');
+let third_Number = prompt('Введите второе число!');
+
+function new_Number(first_Number, second_Number, third_Number) {
+    let rezult = first_Number + second_Number + third_Number;
+    return rezult;
+}
+
+alert(new_Number(first_Number, second_Number, third_Number));
+*/
+//------------------------------------------------------------------------
+
+// Задача №4.
+/*
+let rectangle_Length = prompt('Введите длину прямоугольника!');
+let rectangle_Width = prompt('Введите ширину прямоугольника!');
+
+if (rectangle_Length && rectangle_Width) {
+    function rectangle_Area(rectangle_Length, rectangle_Width) {
+      let rezult = rectangle_Length * rectangle_Width;
+      return rezult;
+    }
+}
+else if (rectangle_Length || rectangle_Width) {
+    function rectangle_Area(rectangle_Length, rectangle_Width) {
+        let rezult = rectangle_Length * rectangle_Length + rectangle_Width * rectangle_Width;
+        return rezult;
+    }
+}
+
+alert(`S = ${rectangle_Area(rectangle_Length, rectangle_Width)}`);
+*/
+//--------------------------------------------------------------------------
+
+// Задача №5.
+ 
+
